1e3caea8abfe8e51fbce8483f0d0fd4502b38780
ESTHERAJUOGU <135168720+ESTHERAJUOGU@users.noreply.github.com>
2024-03-25 16:58:40 +0100

    updated my files,added a new image and a new folder and integrated a javascript file

---
+++ index.js
@@ -0,0 +1,9 @@
+function handleSubmit(event){
+event.preventDefault;
+    alert( "your responseponse has been submitted");
+}
+let reviewInput = document.querySelector("#review-input");
+let review = reviewInput.response.value;
+ let button = document.querySelector("Button");
+button.addEventListener("click", submit);
+            
